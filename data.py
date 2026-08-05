d9175e56d9d04f21dd89c6ebeb74e250e11ffec4
shivam1396 <siddharth10724@gmail.com>
2026-08-05 21:00:45 +0530

    data

---
+++ data.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = np.random.randint(1, 101, size=(5, 3))
+df = pd.DataFrame(data, columns=["A", "B", "C"])
+
+df["Sum"] = df.sum(axis=1)
+df["Average"] = df[["A", "B", "C"]].mean(axis=1)
+
+print(df)
