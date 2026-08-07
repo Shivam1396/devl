21274ea16cacd761d807eaf8acb9a43365525d48
shivam1396 <siddharth10724@gmail.com>
2026-08-07 23:15:22 +0530

    make changes in info

--- Dataqualitypr1/quality.py
+++ Dataqualitypr1/quality.py
@@ -12,12 +12,12 @@ print("\nNumber of duplicate rows:", df.duplicated().sum())
 duplicates = df.duplicated()
 print("duplicates:",duplicates)
 df=df.drop_duplicates()
-# df["Name"]=df["Name"].fillna("Unknown")
+df["Name"]=df["Name"].fillna("Unknown")
 df["DOB"]=df["DOB"].fillna("01-01-1990")
 print(df)
 
 
 
-# print(df.info())
-# print(df.describe())
-# print(df.isnull().sum())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
