11fad78fd020fe4575041240f821c153c766690c
shivam1396 <siddharth10724@gmail.com>
2026-08-20 23:40:58 +0530

    new file added

---
+++ mergesort.js
@@ -0,0 +1,37 @@
+let a = [1, 3, 645, 45]
+let n = a.length
+function mergesort(a, left, right) {
+    if (left < right) {
+        let mid = Math.floor(left + (right - left) / 2)
+
+        mergesort(a, left, mid)       
+        mergesort(a, mid + 1, right) 
+        merge(a, left, mid, right)   
+    }
+}
+function merge(arr, left, mid, right) {
+    let leftArr = arr.slice(left, mid + 1)  
+    let rightArr = arr.slice(mid + 1, right + 1)
+    let i = 0, j = 0, k = left   
+    while (i < leftArr.length && j < rightArr.length) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i]
+            i++
+        } else {
+            arr[k] = rightArr[j]
+            j++
+        }
+        k++
+    }
+    while (i < leftArr.length) {
+        arr[k] = leftArr[i]
+        i++
+        k++
+    }
+    while (j < rightArr.length) {
+        arr[k] = rightArr[j]
+        j++
+        k++
+    }}
+mergesort(a, 0, n - 1)
+console.log(a)  
