9555c4975ab5b248802d9d4625a4f52e89531877
shivam kumar <siddharth10724@gmail.com>
2026-08-25 23:27:41 +0530

    Update insertionsort.js

--- insertionsort.js
+++ insertionsort.js
@@ -2,17 +2,17 @@
 
 //use for loop : when no. of iteration is known 
 //use while loop : when no. of iteration is not known
-let a = [12, 23, 4, 5, 67]
-let n = a.length
+let array = [12, 23, 4, 5, 67]
+let n = array.length
 
 for (let i = 0; i < n; i++) {
-    let key = a[i]      
+    let key = array[i]      
     let j = i - 1
-    while (j >= 0 && a[j] > key) {  
-        a[j + 1] = a[j]
+    while (j >= 0 && array[j] > key) {  
+        array[j + 1] = array[j]
         j--
     }
-    a[j + 1] = key         
+    array[j + 1] = key         
 }
 
-console.log(a)
+console.log(array)
