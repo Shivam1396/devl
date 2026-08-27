a0caa6065eab265d86d6681e8863920061636f6f
shivam kumar <siddharth10724@gmail.com>
2026-08-27 23:48:11 +0530

    Fix dataframe assignment and typo in Sum calculation
    Fix typo in the code and uncomment the dataframe assignment.

--- data.py
+++ data.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 data = np.random.randint(1, 101, size=(5, 3))
 df = pd.DataFrame(data, columns=["A", "B", "C"])
-# df = dataframe
-df["Sum"] = df.sum(axis=1)
+# df = dataframe 
+df["Sum"] = df.sum(axis=1)g
 df["Average"] = df[["A", "B", "C"]].mean(axis=1)
 
 print(df)
