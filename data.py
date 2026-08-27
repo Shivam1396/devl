6b547086cc645096a3a81b8c9e401d95ff8776eb
shivam kumar <siddharth10724@gmail.com>
2026-08-27 23:48:21 +0530

    Fix syntax error in sum calculation

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = np.random.randint(1, 101, size=(5, 3))
 df = pd.DataFrame(data, columns=["A", "B", "C"])
 # df = dataframe 
-df["Sum"] = df.sum(axis=1)g
+df["Sum"] = df.sum(axis=1)
 df["Average"] = df[["A", "B", "C"]].mean(axis=1)
 
 print(df)
